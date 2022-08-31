fade0a927d5a26f71668dcbf9cf1655d571bcb44
Giaape <1kubaape1@gmail.com>
2022-08-31 17:25:28 +0200

    fixed missing url

--- users/urls.py
+++ users/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
                                                 redirect_authenticated_user=True), name='login'),
     path('logout/', views.LogoutViewMy.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
-    path('activate/<uidb64>/<token>/', views.activate_account, name='activate'),
-    path('password-reset/', views.reset_pwd, name='pwd_reset'),
+    path('activate/<uidb64>/<token>/', views.activate_account, name='activate')
+    # path('password-reset/', views.reset_pwd, name='pwd_reset'),
 ]
